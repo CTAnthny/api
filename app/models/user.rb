a108fb31d182fba29bbc0554890629688397db71
TammyTee <TammyTee@users.noreply.github.com>
2017-05-19 20:41:13 -0500

    Added line for GitHub handle validation (#11). (#16)
    * Added line for GitHub handle validation (#11).
    
    * Added line for Codewars handle validation.

--- app/models/user.rb
+++ app/models/user.rb
@@ -24,6 +24,8 @@ class User < ApplicationRecord
   validates_uniqueness_of :email
   validates_uniqueness_of :github_handle, if: Proc.new { |user| user.github_handle.present? }
   validates_presence_of :email, :first_name, :last_name
+  validates :github_handle, :format => { with: /\A[a-zA-Z0-9]*\z/ , :message => 'no special characters, only letters and numbers' }
+  validates :codewars_handle, :format => { with: /\A[a-zA-Z0-9]*\z/ , :message => 'no special characters, only letters and numbers' }
 
   private
 
